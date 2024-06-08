45b7d4ac24abc3b40c9b2d3f95d4865b41c8b6fe
Your Name <you@example.com>
2024-06-08 12:14:14 +0200

    nuova esercitazione

---
+++ Esercitazione 07-06-24/movies.js
@@ -0,0 +1,45 @@
+import {API_KEY} from './keys.js';
+
+
+fetch('https://api.themoviedb.org/3/authentication', {
+    headers: {
+        Authorization: `Bearer ${API_KEY}`
+    }
+})
+.then(res => res.json())
+.then(data => { 
+
+const movies = data.results;
+    displayMovies(movies);
+
+})
+.catch(error => console.error('Errore nel recupero dei dati', error));
+
+
+function displayMovies(movies) {
+    const container = document.querySelector('.movies-container');
+
+
+    movies.forEach(movies => {
+        const card = document.createElement('div');
+        card.className = 'movie-card';
+        const titleEl = document.createElement('h2');
+        titleEl.className = 'movie-title';
+        const image = document.createElement('img');
+        image.className = 'card-img';
+        image.src = movies.image;
+        const description = document.createElement('p');
+        description.className = 'movie-p'
+
+        card.innerHTML = titleEl + image + p;
+
+        container.appendChild(card);
+    })
+}
+
+const searchBar = document.getElementById('searchBar');
+searchBar.addEventListener('input', (event) => {
+    const searchText = event.target.value.toLowerCase();
+    const filteredMovies = movies.filter(movies => movies.titleEl.toLowerCase().includes(searchText));
+    displayMovies(filteredMovies);
+})
