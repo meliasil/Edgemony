d17c4fa6a060fed6db1badf67148fba6ff86acc7
Your Name <you@example.com>
2024-08-01 12:28:34 +0200

    feat: add form, useState and handleSubmit

--- 31-07-24/react-newsletter/src/App.jsx
+++ 31-07-24/react-newsletter/src/App.jsx
@@ -1,14 +1,22 @@
-
-
-
 function App() {
-
-
   return (
     <>
-      <h1>hello world</h1>
+      <main className="bg-neutral-darkSlateGrey">
+        <div className="flex justify-center items-center">
+          <div className="bg-neutral-white w-96 h-96 border-none rounded-2xl p-4">
+            <div className="flex flex-col gap-3">
+              <h1 className="text-neutral-darkSlateGrey font-bold">
+                Stay updated!
+              </h1>
+              <p className="text-neutral-darkSlateGrey">
+                Join 60,000+ product managers receiving monthly updates on:
+              </p>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- 31-07-24/react-newsletter/tailwind.config.js
+++ 31-07-24/react-newsletter/tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          tomato: "hsl(4, 100%, 67%)"
+        },
+        neutral: {
+          darkSlateGrey: "hsl(234, 29%, 20%)",
+          charcoalGrey: "hsl(235, 18%, 26%)",
+          grey: "hsl(231, 7%, 60%)",
+          white: "hsl(0, 0%, 100%)"
+        }
+      }
+    },
   },
   plugins: [],
 }
