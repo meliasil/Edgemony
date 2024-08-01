bf47755fa0927f473b5d06c20d323f37770e0601
Your Name <you@example.com>
2024-08-01 12:29:05 +0200

    feat: add labels

---
+++ 31-07-24/react-newsletter/src/data/labels.js
@@ -0,0 +1,9 @@
+export const labels = {
+    stayUpdated: "Stay Updated!",
+    join: "Join 60,000+ product managers receiving monthly updates on:",
+    list1: "Product discovery and building what matters",
+    list2: "Measuring to ensure updates are a success",
+    list3: "And much more!",
+    emailAddress: "E-mail address",
+    subscribe: "Subscribe to monthly newsletter"
+}
